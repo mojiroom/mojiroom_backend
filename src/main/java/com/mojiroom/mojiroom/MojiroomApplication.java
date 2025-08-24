3316724787355143e97eb5a8edc36e5e159300f3
choing <imgyeong00@gmail.com>
2025-08-24 20:55:24 +0900

    Feat: spring env 설정

---
+++ src/main/java/com/mojiroom/mojiroom/MojiroomApplication.java
@@ -0,0 +1,13 @@
+package com.mojiroom.mojiroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojiroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojiroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mojiroom/mojiroom/MojiroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojiroom.mojiroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojiroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
